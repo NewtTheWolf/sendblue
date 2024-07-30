169948bcf5145c66428981deca497e0d81731e89
Dominik Spitzli <dominik@spitzli.dev>
2024-07-30 12:18:38 +0200

    fix: Add ResponseType to SendableMessage trait

--- src/traits/sendable_message.rs
+++ src/traits/sendable_message.rs
@@ -3,4 +3,5 @@ use serde::Serialize;
 /// Trait for messages that can be sent
 pub trait SendableMessage: Serialize {
     fn endpoint() -> &'static str;
+    type ResponseType: for<'de> serde::Deserialize<'de>;
 }
