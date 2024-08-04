4c5d64b4cb2f5795771c9cc1ec7bfcc529895008
Dominik Spitzli <dominik@spitzli.dev>
2024-08-04 11:59:45 +0200

    feat(send_style): add JsonSchema derivation for SendStyle

--- src/models/send_style.rs
+++ src/models/send_style.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "schemars")]
+use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 /// Style of the message delivery
@@ -10,6 +12,7 @@ use serde::{Deserialize, Serialize};
 /// let style = SendStyle::Celebration;
 /// ```
 #[derive(Serialize, Deserialize, Debug, Clone)]
+#[cfg_attr(feature = "schemars", derive(JsonSchema))]
 #[serde(rename_all = "snake_case")]
 pub enum SendStyle {
     Celebration,
