5effcbf0ca01665335a94d81bae5631fed54b6fe
Dominik Spitzli <dominik@spitzli.dev>
2024-09-24 11:07:08 +0200

    feat: adjusted types

--- src/model/evaluate_service.rs
+++ src/model/evaluate_service.rs
@@ -14,72 +14,35 @@ use schemars::JsonSchema;
 /// # Examples
 ///
 /// ```
-/// use sendblue::models::EvaluateServiceBuilder;
+/// use sendblue::models::EvaluateService;
 ///
-/// let request = EvaluateServiceBuilder::new()
-///     .number(phonenumber::parse(None, "+19999999999").unwrap())
-///     .build();
+/// let request = EvaluateService::new(PhoneNumber::new("+1234567890").unwrap());
 /// ```
 #[derive(Serialize, Deserialize, Debug)]
 pub struct EvaluateService {
     pub number: PhoneNumber,
 }
 
-/// Builder for creating an `EvaluateService` request
-///
-/// # Examples
-///
-/// ```
-/// use sendblue::models::EvaluateServiceBuilder;
-///
-/// let request = EvaluateServiceBuilder::new()
-///     .number(phonenumber::parse(None, "+19999999999").unwrap())
-///     .build();
-/// ```
-pub struct EvaluateServiceBuilder {
-    number: Option<PhoneNumber>,
-}
-
-impl EvaluateServiceBuilder {
-    pub fn new() -> Self {
-        Self { number: None }
-    }
-
-    pub fn number(mut self, number: PhoneNumber) -> Self {
-        self.number = Some(number);
-        self
-    }
-
-    pub fn build(self) -> EvaluateService {
-        EvaluateService {
-            number: self.number.expect("Number is required"),
-        }
+impl EvaluateService {
+    pub fn new(number: PhoneNumber) -> Self {
+        Self { number }
     }
 }
 
-impl Default for EvaluateServiceBuilder {
-    fn default() -> Self {
-        Self::new()
-    }
+/// Enum for the type of service that can be evaluated
+#[derive(Serialize, Deserialize, Debug)]
 #[cfg_attr(feature = "schemars", derive(JsonSchema))]
 #[serde(rename_all = "camelCase")]
+pub enum EvaluateServiceType {
+    IMessage,
+    SMS,
 }
 
 /// Response from the Sendblue API for evaluating if a number can send/receive iMessages
-///
-/// # Examples
-///
-/// ```
-/// use sendblue::models::EvaluateServiceResponse;
-///
-/// let response = EvaluateServiceResponse {
-///     number: "+19999999999".into(),
-///     service: "iMessage".into(),
-/// };
-/// ```
 #[derive(Serialize, Deserialize, Debug)]
 #[cfg_attr(feature = "schemars", derive(JsonSchema))]
 pub struct EvaluateServiceResponse {
-    pub number: String,
-    pub service: String,
+    #[serde(deserialize_with = "deserialize_phone_number")]
+    pub number: PhoneNumber,
+    pub service: EvaluateServiceType,
 }
