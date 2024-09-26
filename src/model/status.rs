2f9e7ace8351091dab5be2032fa97d5379067bc9
Dominik Spitzli <dominik@spitzli.dev>
2024-09-26 15:49:34 +0200

    docs: better docs

--- src/model/status.rs
+++ src/model/status.rs
@@ -23,20 +23,35 @@ pub enum Status {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[cfg_attr(feature = "schemars", derive(JsonSchema))]
 pub enum ErrorCode {
+    /// Validation Error: see error_message field (Code: 4000)
     #[serde(rename = "4000")]
     ValidationError,
+
+    /// Rate Limit Exceeded (Code: 4001)
     #[serde(rename = "4001")]
     RateLimitExceeded,
+
+    /// Blacklisted Number (e.g., emergency number like 911) (Code: 4002)
     #[serde(rename = "4002")]
     BlacklistedNumber,
+
+    /// Internal Error (Code: 5000)
     #[serde(rename = "5000")]
     InternalError,
+
+    /// Server Rate Exceeded (Code: 5003)
     #[serde(rename = "5003")]
     ServerRateExceeded,
+
+    /// Message failed to send (Code: 10001)
     #[serde(rename = "10001")]
     MessageFailedToSend,
+
+    /// Failed to resolve message status (Code: 10002)
     #[serde(rename = "10002")]
     FailedToResolveMessageStatus,
+
+    /// Unknown error code
     #[serde(other)]
     Unknown,
 }
